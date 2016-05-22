e1dd1c9910f5fb9aa9b8fad87bbde4e9ab49c52f
Varun Tolani <vatolani@gmail.com>
2016-05-22 16:42:35 -0300

    mongo cambio para heroku

--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
 var uristring =
-  process.env.MONGOLAB_URI ||
+  process.env.MONGODB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/angular-book';
 
