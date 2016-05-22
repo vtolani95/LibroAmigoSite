e25b0f319a543ce5e02429f4c1c62e830790988e
Varun Tolani <vatolani@gmail.com>
2016-05-22 16:40:17 -0300

    cambios de mongodb para heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'jade');
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
 var uristring =
-  process.env.MONGOLAB_URI ||
+  process.env.MONGODB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/angular-book';
 
